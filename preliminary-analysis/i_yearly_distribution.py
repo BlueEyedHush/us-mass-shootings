de74c381a476e932034de622bde7d52731a70a07
knawara <blueeyedhush@gmail.com>
2017-11-09 10:22:28 +0100

    visual improvements to interactive plot

--- preliminary-analysis/i_yearly_distribution.py
+++ preliminary-analysis/i_yearly_distribution.py
@@ -1,6 +1,7 @@
 
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider, Button, RadioButtons
+from math import floor, ceil
 
 from data_proc import *
 
@@ -11,33 +12,45 @@ bmp['count'] = 1
 
 ubins = bins.unique()[::-1]
 
+def bin_text_repr(b):
+	nlow = int(ceil(b.left))
+	nhigh = int(floor(b.right))
+	return "[{}, {}]".format(nlow, nhigh)
+bins_txt = map(bin_text_repr, ubins)
+
 def for_year_bin(bin_id):
 	bmpy = bmp[bmp['db'] == ubins[bin_id]]
 	by_month = bmpy.groupby(['month']).sum()
 	by_month = by_month[['Fatalities', 'Injured', 'Total victims', 'count']]
 	return by_month
 
-def plot(axis, data):
+fig, ax = plt.subplots()
+plt.subplots_adjust(bottom=0.25)
+
+# (h_left, v_left, len, height)
+ax_year = plt.axes([0.15, 0.1, 0.65, 0.03])
+s_year = Slider(ax_year, 'Year', 0, len(ubins) - 0.1, valfmt = "%d")
+
+def plot(bin_id):
+	data = for_year_bin(bin_id)
+
 	ax.clear()
 	
 	ax.set_xticks(range(1,13))
 	ax.set_xlim([0,13])
 
+	s_year.valtext.set_text(bins_txt[bin_id])
+
 	ax.bar(data.index, data['count'])
 	fig.canvas.draw_idle()
 
-fig, ax = plt.subplots()
-bm = for_year_bin(0)
-plot(ax, bm)
-ax_year = plt.axes([0.0, 0.0, 0.98, 0.05])
-s_year = Slider(ax_year, 'Year int', 0, len(ubins))
-
 def update(val):
 	bin_id = int(s_year.val)
-	bm = for_year_bin(bin_id)
-	plot(ax, bm)
+	plot(bin_id)
 
 s_year.on_changed(update)
+
+plot(0)
 
 fig.canvas.draw()
 plt.show()
