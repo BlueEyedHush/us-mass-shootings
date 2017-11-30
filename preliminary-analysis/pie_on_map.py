dea92c782c7a3c4882840270cafba77d0f18f586
blueeyedhush <blueeyedhush@gmail.com>
2017-11-30 12:44:04 +0100

    pie charts on top of map

--- preliminary-analysis/pie_on_map.py
+++ preliminary-analysis/pie_on_map.py
@@ -1,98 +1,61 @@
 from __future__ import print_function
 import numpy as np
+import math
 import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap as Basemap
-from matplotlib.colors import rgb2hex
 from matplotlib.patches import Polygon
 from us_states_coords import get_coords_map
 
-us_state_center = get_coords_map()
+colors = ['red','blue','green','yellow','magenta','purple']
+
+def draw_pie(ax, ratios=[0.4, 0.3, 0.3], X=0, Y=0, size=1000):
+    N = len(ratios)
+
+    xy = []
+
+    start = 0.
+    for ratio in ratios:
+        x = [0] + np.cos(np.linspace(2 * math.pi * start, 2 * math.pi * (start + ratio), 30)).tolist()
+        y = [0] + np.sin(np.linspace(2 * math.pi * start, 2 * math.pi * (start + ratio), 30)).tolist()
+        xy1 = zip(x, y)
+        xy.append(xy1)
+        start += ratio
+
+    for i, xyi in enumerate(xy):
+        fc = colors[i]
+        ax.scatter([X], [Y], marker=(xyi, 0), s=size, facecolor=fc, zorder=4)
+
+
+
+fig = plt.figure()
+
+plt.subplots_adjust(left=0.05,right=0.95,top=0.90,bottom=0.05,wspace=0.15,hspace=0.05)
+sbp_ax = plt.subplot(111)
 
 # Lambert Conformal map of lower 48 states.
 m = Basemap(llcrnrlon=-119,llcrnrlat=22,urcrnrlon=-64,urcrnrlat=49,
             projection='lcc',lat_1=33,lat_2=45,lon_0=-95)
-# draw state boundaries.
-# data from U.S Census Bureau
-# http://www.census.gov/geo/www/cob/st2000.html
+
+
 shp_info = m.readshapefile('st99_d00','states',drawbounds=True)
-# population density by state from
-# http://en.wikipedia.org/wiki/List_of_U.S._states_by_population_density
-
-popdensity = {
-'New Jersey':  438.00,
-'Rhode Island':   387.35,
-'Massachusetts':   312.68,
-'Connecticut':	  271.40,
-'Maryland':   209.23,
-'New York':    155.18,
-'Delaware':    154.87,
-'Florida':     114.43,
-'Ohio':	 107.05,
-'Pennsylvania':	 105.80,
-'Illinois':    86.27,
-'California':  83.85,
-'Hawaii':  72.83,
-'Virginia':    69.03,
-'Michigan':    67.55,
-'Indiana':    65.46,
-'North Carolina':  63.80,
-'Georgia':     54.59,
-'Tennessee':   53.29,
-'New Hampshire':   53.20,
-'South Carolina':  51.45,
-'Louisiana':   39.61,
-'Kentucky':   39.28,
-'Wisconsin':  38.13,
-'Washington':  34.20,
-'Alabama':     33.84,
-'Missouri':    31.36,
-'Texas':   30.75,
-'West Virginia':   29.00,
-'Vermont':     25.41,
-'Minnesota':  23.86,
-'Mississippi':	 23.42,
-'Iowa':	 20.22,
-'Arkansas':    19.82,
-'Oklahoma':    19.40,
-'Arizona':     17.43,
-'Colorado':    16.01,
-'Maine':  15.95,
-'Oregon':  13.76,
-'Kansas':  12.69,
-'Utah':	 10.50,
-'Nebraska':    8.60,
-'Nevada':  7.03,
-'Idaho':   6.04,
-'New Mexico':  5.79,
-'South Dakota':	 3.84,
-'North Dakota':	 3.59,
-'Montana':     2.39,
-'Wyoming':      1.96,
-'Alaska':     0.42}
-print(shp_info)
-
-# choose a color for each state based on population density.
-colors={}
-statenames=[]
-cmap = plt.cm.hot # use 'hot' colormap
-vmin = 0; vmax = 450 # set range.
-print(m.states_info[0].keys())
-for shapedict in m.states_info:
-    statename = shapedict['NAME']
-    # skip DC and Puerto Rico.
-    if statename not in ['District of Columbia','Puerto Rico']:
-        lat, long = us_state_center[statename]
-        x,y = m(long,lat)
-        m.plot(x,y,'bo', markersize=20)
-
-    statenames.append(statename)
-# cycle through state names, color each one.
 ax = plt.gca() # get current axes instance
+
 for nshape,seg in enumerate(m.states):
     poly = Polygon(seg,facecolor='white')
     ax.add_patch(poly)
 # draw meridians and parallels.
 m.drawparallels(np.arange(25,65,20),labels=[1,0,0,0])
 m.drawmeridians(np.arange(-120,-40,20),labels=[0,0,0,1])
+
 plt.title('Filling State Polygons by Population Density')
+
+us_state_center = get_coords_map()
+for shapedict in m.states_info:
+    statename = shapedict['NAME']
+    # skip DC and Puerto Rico.
+    if statename not in ['District of Columbia','Puerto Rico']:
+        lat, long = us_state_center[statename]
+        x,y = m(long,lat)
+        draw_pie(sbp_ax, X=x, Y=y)
+
 plt.show()
